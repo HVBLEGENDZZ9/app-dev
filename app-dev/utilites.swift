74c23102ecb8e2827ea7be34567859b3f7895674
HVBLEGENDZZ9 <harshvardhanb20@cse.iiitp.ac.in>
2024-07-04 18:20:44 +0530

    text changes

--- app-dev/utilites.swift
+++ app-dev/utilites.swift
@@ -11,10 +11,10 @@ import SwiftUI
 func greetingText() -> String{
     let hour = Calendar.current.component(.hour, from: Date())
     switch hour {
-    case 6..<12 : return "Good Morning!"
-    case 12..<17 : return "Good Afternoon!"
-    case 17..<22 : return "Good Evening!"
-    default: return "Good Evening!"
+    case 6..<12 : return "Good Morning"
+    case 12..<17 : return "Good Afternoon"
+    case 17..<22 : return "Good Evening"
+    default: return "Good Evening"
     }
 }
 
